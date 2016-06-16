5586a8753eca6038aad767c58af78e88b8fe1328
Joel W. King <joel.king@wwt.com>
2016-06-16 10:14:53 -0400

    Rulename base on source IP address

--- f5_firewall/f5_firewall_playbook.py
+++ f5_firewall/f5_firewall_playbook.py
@@ -8,6 +8,8 @@ import json
 #
 #      author: Joel W. King,  World Wide Technology
 #
+#      revisions:
+#      16 June 2016  | Changes to parameters, create rule name base on the source IP
 #
 def block_IP_cb(action, success, container, results, handle):
 
@@ -21,12 +23,14 @@ def on_start(container):
  
     ips = set(phantom.collect(container, 'artifact:*.cef.sourceAddress'))
 
-    boiler_plate = { "action" : "reject",  "policy" : "Phantom_Inbound",  "rule name" : "Phantom_DYNAMIC_BLOCK",  "partition" : "Common"}
+    boiler_plate = { "action" : "reject",  "policy" : None,  "rule name" : None,  "partition" : "Common"}
 
     for ip in ips:
         
         parameters = boiler_plate
-        parameters["source"] = ip
+        parameters["policy"] = "Phantom_Inbound"        # Policy name must exist on the F5 BIG-IP
+        parameters["source"] = ip                       # Source IP we are blocking
+        parameters["rule name"] = "Phantom" + ip        # Make the rule name based on the source IP address
         phantom.debug("PARAMETERS \n%s" % parameters)
         phantom.act('block ip', parameters=[parameters], assets=["f5"], callback=block_IP_cb)
 
@@ -37,4 +41,3 @@ def on_finish(container, summary):
     phantom.debug("Summary: " + summary)
 
     return
-
